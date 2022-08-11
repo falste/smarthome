b2cb167a22f785b9a875f54cb561f9eb52a835dc
falste <fal.stephan@gmail.com>
2022-08-11 23:41:49 +0200

    Set up basic daemon skeleton

---
+++ inc/daemon.h
@@ -0,0 +1,32 @@
+#ifndef DAEMON_H_
+#define DAEMON_H_
+
+#include <csignal>
+#include <functional>
+
+class Daemon {
+   public:
+    static Daemon& instance() {
+        static Daemon instance;
+        return instance;
+    }
+
+    void setReloadFunction(std::function<void()> func);
+
+    bool IsRunning();
+
+   private:
+    std::function<void()> m_reloadFunc;
+    bool m_isRunning;
+    bool m_reload;
+
+    Daemon();
+    Daemon(Daemon const&) = delete;
+    void operator=(Daemon const&) = delete;
+
+    void Reload();
+
+    static void signalHandler(int signal);
+};
+
+#endif  // DAEMON_H_
